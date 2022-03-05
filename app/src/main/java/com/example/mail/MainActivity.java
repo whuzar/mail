e00e23e7b1f05fcc4e9592f7c7df446b83a53f23
wojte <wojtekhuzar@gmail.com>
2022-03-05 17:24:43 +0100

    Zmiany

--- app/src/main/java/com/example/mail/MainActivity.java
+++ app/src/main/java/com/example/mail/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
